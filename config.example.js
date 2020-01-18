856a6cdf7ce0226c7d0f2c18c6d81bebefdb19bc
Kipras Melnikovas <kipras@kipras.org>
2020-01-18 19:05:30 +0200

    Create `config.example.js`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ config.example.js
@@ -0,0 +1,16 @@
+/**
+ * EN |
+ * Warning - this file should be kept private.
+ * Copy & rename it to `config.js`
+ *
+ * LT |
+ * Dėmesio - šis failas turi būti privatus.
+ * Nukopijuok ir pervadink jį į `config.js`
+ *
+ */
+
+export default {
+	username: "",
+	password: "",
+	identificationNumber: "",
+};
