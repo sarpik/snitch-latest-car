6121ca14ad43ea700ec1ea821d4d42e4bf94d4e9
Kipras Melnikovas <kipras@kipras.org>
2020-01-18 22:41:50 +0200

    Create the `latestCarSnitcher` utility (big!) 🔥
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ latestCarSnitcher.js
@@ -0,0 +1,133 @@
+// latestCarSnitcher.js
+/**
+ * Copyright © Kipras Melnikovas (https://kipras.org) <kipras@kipras.org>
+ */
+
+/* eslint-disable @typescript-eslint/no-use-before-define */
+
+import puppeteer from "puppeteer";
+import config from "./config.js";
+
+const url = "https://www.vaurioajoneuvo.fi/";
+const recentlyAddedVehiclesUrl = "https://www.vaurioajoneuvo.fi/?mod=vehicle&act=lastest";
+
+/**
+ * Workflow:
+ *
+ * 0. launch the browser
+ * 1. go to the main url
+ * 2. authenticate
+ * 3. Go to the latest update car page
+ * 4. get the latest car's ~~image~~ ID
+ * 5. compare it with the stored one
+ * 5.1 if they          are     equal - refresh page & go to step 4.
+ * 5.2 otherwise - they are NOT equal - store it as the latest car's ID
+ * 6. scroll down to the bottom of the car's info page
+ * 7. click the "buy now" button to navigate to another page
+ * 8. click on the input field for "identification number"
+ * 9. paste the identification number (always the same)
+ * 10. click "continue shopping" - the car is reserved for 5 minutes
+ * 11. click "go back"
+ * 12?. notify the user that we've captured something
+ * 13. exit OR repeat?
+ *
+ * NOTE
+ * if a selector does not work, make sure it's unique
+ * if it's not, use the full xpath instead.
+ *
+ * @returns {Promise<void>}
+ */
+export const latestCarSnitcher = async () => {
+	const browser = await puppeteer.launch({
+		headless: config.headless,
+		// slowMo: 500,
+	});
+
+	const page = await browser.newPage();
+	await page.goto(url);
+
+	await authenticate(page, config.username, config.password);
+
+	await page.goto(recentlyAddedVehiclesUrl);
+
+	/** @type {(number|null)} */
+	let latestVehicleId = null;
+
+	while (true) {
+		const currentVehicleId = await getCurrentVehicleId(page);
+
+		if (currentVehicleId === latestVehicleId) {
+			/** nothing new - refresh & try again */
+			await page.reload();
+
+			/** TODO - add some timeout maybe? */
+			continue;
+		}
+
+		latestVehicleId = currentVehicleId;
+		break;
+	}
+
+	console.log("latestVehicleId", latestVehicleId);
+
+	/** NOTE there are two buttons with duplicate id's, so you cannot use an ID to select'em */
+	// // const buyNowButtonSelector = "#submit_trans";
+	// // await page.waitForXPath(
+	// // 	"/html/body/div[2]/div[3]/div[2]/div/div[2]/div[2]/table/tbody/tr[2]/td/div/table/tbody/tr[16]/td/div/div/div[2]/input"
+	// // );
+	// // console.log("waiting done");
+	// // await page.click(buyNowButtonFullXPath);
+
+	const buyNowButtonFullXPath =
+		"/html/body/div[2]/div[3]/div[2]/div/div[2]/div[2]/table/tbody/tr[2]/td/div/table/tbody/tr[16]/td/div/div/div[2]/input";
+
+	await page.waitForXPath(buyNowButtonFullXPath); /** not necessary atm */
+	const buyNowButtonElement = (await page.$x(buyNowButtonFullXPath))[0];
+	await buyNowButtonElement.click();
+
+	/**  */
+	const identificationNumberInputSelector = "#b_ssn";
+
+	await page.waitFor(identificationNumberInputSelector);
+	await page.focus(identificationNumberInputSelector);
+	await page.keyboard.type(config.identificationNumber.toString());
+
+	/**  */
+	const continueShoppingButtonSelector = "#showcontent > div:nth-child(4) > form > div > input";
+
+	await page.waitFor(continueShoppingButtonSelector); /** not necessary atm */
+	await page.click(continueShoppingButtonSelector);
+
+	/**  */
+	const goBackButtonSelector =
+		"#showcontent > div:nth-child(3) > table > tbody > tr:nth-child(8) > td > div > input:nth-child(1)";
+
+	await page.waitFor(goBackButtonSelector);
+	await page.click(goBackButtonSelector);
+
+	// /**
+	//  * Click on some image to open the full description
+	//  */
+	// // const cardAHrefSelector = "#vehhinfo > a";
+	// // await page.click(cardAHrefSelector);
+	// const aHrefFullXPath = "/html/body/div[2]/div[3]/div[2]/div/div[1]/div[2]/div/table/tbody/tr[1]/td[2]/div/div[4]/a";
+	// const href = await getPropertyStrByXPath(page, aHrefFullXPath, "href");
+
+	// console.log("href", href);
+
+	// await page.goto(href, {
+	// 	/**
+	// 	 * https://stackoverflow.com/a/46217285/9285308
+	// 	 * & https://github.com/puppeteer/puppeteer/blob/master/docs/api.md#pagegotourl-options
+	// 	 */
+	// 	waitUntil: "networkidle2",
+	// });
+
+	// eslint-disable-next-line no-extra-boolean-cast
+	if (!!config.headless || !!config.shouldCloseBrowserOnceDone) {
+		await page.close({});
+		await browser.close();
+	}
+
+	return;
+};
