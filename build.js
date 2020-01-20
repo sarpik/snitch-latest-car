d35229dcbe44627ea6c33bc674968f9d06fd80de
Kipras Melnikovas <sarpixo@gmail.com>
2020-01-20 20:51:59 +0200

    Document the sh1t outta `build.js`
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

--- build.js
+++ build.js
@@ -1,13 +1,52 @@
 // build.js
 
 /**
- * you could use
+ * This is supposed to be used as an API
+ * for building the application itself.
  *
- * ```sh
- * yarn pkg ./go --out-path ./dist && cp -r ./node_modules/puppeteer/.local-chromium ./dist/puppeteer
+ * Copyright © Kipras Melnikovas (https://kipras.org) <kipras@kipras.org>
+ *
+ * IMPORTANT
+ * `./installer.js` depends on this, but indirectly (even more important).
+ *
+ * TL;DR:
+ * DO NOT INTRODUCE BREAKING CHANGES TO THIS FILE
+ * if you want to remain backwards-compatible
+ * with previously compiled `installer` binaries.
+ *
+ * Breaking changes would include, but are not limited to:
+ * * Renaming the file
+ * * Changing it's core behaviour (building the application)
+ * * Not using `export default` for the main `build` function
+ *
+ * Explanation:
+ *
+ * Previously compiled binary executables will download the latest sources
+ * from a remote repository and will point to `<downloaded-repository-path>/build.js`
+ *
+ * The `installer.js` (and thus the compiled `installer` binaries) do *not* use
+ *
+ * ```js
+ * const build = require("./build.js");
  * ```
  *
- * but this is cross-platform (hopefully?)
+ * instead, they use
+ *
+ * ```js
+ * const build = require(path.join(process.cwd(), "build.js"));
+ * ```
+ *
+ * because `installer.js` is meant to be *compiled to* and *used as* a binary executable!
+ *
+ * This allows the binary to download the latest source files
+ * from the remote repository and **use** the downloaded `build.js` script!
+ *
+ * Otherwise it'd be pointless to download the latest sources,
+ * since the `build.js` script might be outdated,
+ * making the binary useless until it's recompiled
+ * against the latest `build.js` script --
+ * and that's what we are avoiding this way.
+ *
  */
 
 const fs = require("fs-extra");
