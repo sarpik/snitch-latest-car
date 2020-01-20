72dc466033af84b73d9c038507069f8a54c0b0f4
Kipras Melnikovas <sarpixo@gmail.com>
2020-01-20 20:43:56 +0200

    Create `build-installer-pkg.sh`
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

---
+++ build-installer-pkg.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# build-installer.bash
+#
+# I've also tried `nexe/nexe`, but came across an issue:
+# https://github.com/nexe/nexe/issues/719
+#
+
+outDir="installer-pkg"
+fileNameTemplate="$outDir/installer"
+
+rm -rf "$outDir" && yarn pkg ./package.json --output "$fileNameTemplate" --target "node12-linux,node12-win,node12-macos"
