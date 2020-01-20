05ed872425180ff3455efe9e02a520fd990e037f
Kipras Melnikovas <sarpixo@gmail.com>
2020-01-20 20:50:30 +0200

    Print warning & exit @ `build-installer.sh`
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

--- build-installer.sh
+++ build-installer.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 # build-installer.bash
+#
+# WARNING the executable will be broken if you're building on windows
+# See https://github.com/nexe/nexe/issues/719
+#
+
+printf "
+WARNING - building on windows will result in a broken executable!
+The issue is described here: https://github.com/nexe/nexe/issues/719
+
+use ./build-installer-pkg.sh instead!
+"
+
+exit 1 # comment this line out if you want to test this script for yourself
 
 target=${1:-"12.9.1"}
 
